a36a133b9c08fe01ff1bb7582b55be977016420b
AgustinCragno <cragno.agustin@gmail.com>
2023-07-19 15:13:16 -0300

    Removed unnecessary class PropertiesManager

--- app/src/main/java/com/disfluency/MainActivity.kt
+++ app/src/main/java/com/disfluency/MainActivity.kt
@@ -3,9 +3,9 @@ package com.disfluency
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.disfluency.api.PropertiesReader
 import com.disfluency.navigation.AppNavigation
 import com.disfluency.ui.theme.DisfluencyTheme
+import com.disfluency.utilities.PropertiesReader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/disfluency/utilities/PropertiesReader.kt
+++ app/src/main/java/com/disfluency/utilities/PropertiesReader.kt
@@ -6,22 +6,11 @@ import java.util.*
 private const val CONFIG_FILE = "application.properties"
 
 object PropertiesReader {
-    private var manager: PropertiesManager? = null
+    private var properties: Properties? = null
 
     fun initialize(context: Context){
-        manager = PropertiesManager(context)
-    }
-    fun getProperty(key: String): String = manager!!.get(key)
-}
-
-private class PropertiesManager(context: Context){
-    var properties: Properties
-        private set
-
-    init {
         properties = Properties()
-        properties.load(context.assets.open(CONFIG_FILE))
+        properties?.load(context.assets.open(CONFIG_FILE))
     }
-
-    fun get(key: String): String = properties.getProperty(key)
+    fun getProperty(key: String): String = properties!!.getProperty(key)
 }
