a2c45586683d6d272cdeab847accc20ebd4f3a04
josebruzzoni <bruzzonijose@gmail.com>
2023-10-18 16:27:04 -0300

    results

--- app/src/main/java/com/disfluency/components/charts/BarChart.kt
+++ app/src/main/java/com/disfluency/components/charts/BarChart.kt
@@ -2,6 +2,8 @@ package com.disfluency.components.charts
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
